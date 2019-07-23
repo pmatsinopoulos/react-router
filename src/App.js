d2f5764bbf20d7681cb17df2bcde195d7072ac75
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-07-23 18:18:09 +0300

    Using a Switch makes sense for having a 404 content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const History = () => <h1>My History</h1>
 const About = () => <h1>About</h1>
@@ -11,9 +11,12 @@ const Contact = () => <h1>Contact</h1>
 function App() {
   return (
     <div className="App">
-      <Route path="/history" component={History}/>
-      <Route path="/about" component={About}/>
-      <Route path="/contact" component={Contact}/>
+        <Switch>
+          <Route path="/history" component={History}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
+          <Route component={() => <h1>404</h1>}/>
+        </Switch>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
