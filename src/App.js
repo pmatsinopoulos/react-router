99b96f899752cda1b17392c1021cd37e3ea48f70
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-07-23 18:35:25 +0300

    Redirect demo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch, Link, Redirect } from 'react-router-dom';
 
 const Home = () => (
   <div>
     <h1>Home</h1>
-    <Link to="/about">About</Link>
+    <Link to="/about">About</Link><br/>
+    <Link to="/go_back">Go Back</Link>
   </div>
 )
 const About = () => (
   <div>
     <h1>About</h1>
-    <Link to="/">Home</Link>
+    <Link to="/">Home</Link><br/>
+    <Link to="/go_back">Go Back</Link>
   </div>
 )
-
+const GoBack = () => <Redirect to="/"/>
 
 function App() {
   return (
@@ -26,6 +28,7 @@ function App() {
             path="/about"
             component={About} />}
           />
+          <Route path="/go_back" component={GoBack}/>
 
           <Route component={() => <h1>404</h1>}/>
         </Switch>
