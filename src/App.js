cd4a681f750806a4ea3daf8147adf0973b5e0bb3
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-07-23 18:04:25 +0300

    First <Route> example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+
+const MyHistory = () => (
+  <h1>My History</h1>
+)
 
 function App() {
   return (
     <div className="App">
+      <Route path="/history" component={MyHistory}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
