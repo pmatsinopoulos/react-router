630f682585994e97b5e8f5018f6549ff7f01e36f
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-07-23 18:05:39 +0300

    3 Routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 
-const MyHistory = () => (
-  <h1>My History</h1>
-)
+const History = () => <h1>My History</h1>
+const About = () => <h1>About</h1>
+const Contact = () => <h1>Contact</h1>
+
 
 function App() {
   return (
     <div className="App">
-      <Route path="/history" component={MyHistory}/>
+      <Route path="/history" component={History}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
