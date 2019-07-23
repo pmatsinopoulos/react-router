8a276e20247aade975fb67525f8ffe85bcb935ce
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-07-23 21:15:39 +0300

    URL parameters

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route, Switch, Link, NavLink, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-    <NavLink to="/about" activeClassName="foo">About</NavLink><br/>
-    <Link to="/go_back">Go Back</Link>
-  </div>
-)
-const About = () => (
-  <div>
-    <h1>About</h1>
-    <NavLink to="/" activeClassName="foo">Home</NavLink><br/>
-    <Link to="/go_back">Go Back</Link>
-  </div>
-)
-const GoBack = () => <Redirect to="/"/>
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route
-            path="/about"
-            component={About} />}
-          />
-          <Route path="/go_back" component={GoBack}/>
+    <Router>
+      <div>
+        <h2>Accounts</h2>
 
-          <Route component={() => <h1>404</h1>}/>
-        </Switch>
+        <ul>
+          <li>
+            <Link to="/netflix">Netflix</Link>
+          </li>
 
-    </div>
-  );
+          <li>
+            <Link to="/zillow-group">Zillow Group</Link>
+          </li>
+
+          <li>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+
+          <li>
+            <Link to="/modus-create">Modus Create</Link>
+          </li>
+        </ul>
+
+        <Route path='/:id' component={Child}/>
+
+        <Route
+            path="/order/:direction(asc|desc)"
+            component={ComponentWithRegex}
+            />
+
+      </div>
+    </Router>
+  )
 }
+
+const Child = ({match}) => (
+  <div>
+    <h3>ID: {match.params.id}</h3>
+  </div>
+)
+
+const ComponentWithRegex = ({match}) => (
+  <div>
+    <h3>ASC or DESC: {match.params.direction}</h3>
+  </div>
+)
 
 export default App;
