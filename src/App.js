d4fc23fe21b5de92f808e8cf5c7bdefd932b6229
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-07-23 18:32:01 +0300

    Link component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
-const History = () => <h1>My History</h1>
-const About = (props) => <h1>About {props.extra}</h1>
-const Contact = () => <h1>Contact</h1>
-const Home = () => <h1>Home</h1>
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <Link to="/about">About</Link>
+  </div>
+)
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <Link to="/">Home</Link>
+  </div>
+)
 
 
 function App() {
-  const someVariable = 'peter';
-
   return (
     <div className="App">
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route
             path="/about"
-            render={props => <About {...props} extra={someVariable} />}
+            component={About} />}
           />
 
           <Route component={() => <h1>404</h1>}/>
