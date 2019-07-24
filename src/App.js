8874d95b4dddd4f944cdda1a233d2b11202f9136
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-07-24 13:00:32 +0300

    Route configuration

--- src/App.js
+++ src/App.js
@@ -1,67 +1,84 @@
 import React from 'react';
-import { BrowserRouter as Router,
-         Route,
-         Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Sandwitches = () => <h3>Sandwitches</h3>
+const Tacos = ({routes}) => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/tacos/bus">Bus</Link>
+      </li>
+      <li>
+        <Link to="/tacos/cart">Cart</Link>
+      </li>
+    </ul>
+    {routes.map((route, index) => (
+        <RouteWithSubRoutes key={index} {...route} />
+      )
+    )}
+  </div>
+)
+const Cart = () => <h3>Cart</h3>
+const Bus = () => <h3>Bus</h3>
 
 const routes = [
   {
-    path: "/",
-    exact: true,
-    sidebar: () => <div>Home!</div>,
-    main: () => <h2>Home</h2>
+    path: "/sandwiches",
+    exact: false,
+    component: Sandwitches
   },
   {
-    path: "/bubblegum",
+    path: "/tacos",
     exact: false,
-    sidebar: () => <div>Bubblegum!</div>,
-    main: () => <h2>Bubblegum</h2>
-  },
-  {
-    path: "/shoelaces",
-    exact: false,
-    sidebar: () => <div>Show laces!</div>,
-    main: () => <h2>Show laces</h2>
+    component: Tacos,
+    routes: [
+      {
+        path: "/tacos/bus",
+        exact: true,
+        component: Bus
+      },
+      {
+        path: "/tacos/cart",
+        exact: true,
+        component: Cart
+      }
+    ]
   }
 ]
+
+const RouteWithSubRoutes = (route) => (
+  <Route
+    path={route.path}
+    exact={route.exact}
+    render={props => {
+      console.debug("props in render of RouteWithSubRoutes", props);
+      return (
+        // pass the sub-routes down in order to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}}
+  />
+)
 
 const App = () => (
   <Router>
     <div>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/bubblegum">BubbleGum</Link>
-          </li>
-          <li>
-            <Link to="/shoelaces">Shoe Laces</Link>
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <Link to="/tacos">Tacos</Link>
+        </li>
 
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.sidebar}
-          />
-         ))}
-      </div>
+        <li>
+          <Link to="/sandwiches">Sandwiches</Link>
+        </li>
+      </ul>
 
-      <div>
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            component={route.main}
-            path={route.path}
-            exact={route.exact}
-          />
-        ))}
-      </div>
+      {routes.map((route, index) => (
+        <RouteWithSubRoutes key={index} {...route} />
+      ))}
     </div>
   </Router>
 )
 
 export default App;
+
+
